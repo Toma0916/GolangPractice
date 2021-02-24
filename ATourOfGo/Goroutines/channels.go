e5645af6d91c1b2757ccea6293b9e961beee219a
鈴木透馬 <yiyiaxpb@gmail.com>
2021-02-24 11:56:01 +0900

    Add gorutine.

---
+++ ATourOfGo/Goroutines/channels.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+// channelは並列実行された関数間における値の送受信を提供する型
+// 宣言はチャンネルで扱う型とともに行う
+// ちなみにgorutineは非同期処理にするために戻り値を書けない
+
+func sum(s []int, c chan int) {
+	sum := 0
+	for _, v := range s {
+		sum += v
+	}
+	c <- sum // send sum to c
+}
+
+func main() {
+	s := []int{7, 2, 8, -9, 4, 0}
+
+	c := make(chan int)
+	go sum(s[:len(s)/2], c)
+	go sum(s[len(s)/2:], c)
+	x := <-c
+	y := <-c // receive from c
+	fmt.Println(x, y, x+y)
+}
---
+++ ATourOfGo/Goroutines/gorutines.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func say(s string) {
+	for i := 0; i < 5; i++ {
+		time.Sleep(100 * time.Millisecond)
+		fmt.Println(s)
+	}
+}
+
+func main() {
+	// go f(x, y ,z)と書くと新しいgorutineが実行される
+	// f, x, y, xの評価は実行元のgorutineで行われ、fの実行は新しいgorutineで実行される
+	// gorutineは同じアドレス空間で実行されるため共有メモリへのアクセスは必ず同期する必要がある
+	go say("Hello")
+	say("Hello")
+}
